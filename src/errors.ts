95e1becf4abbf0b866db8088037448a3c27892f6
Paulo Tinoco <paulovictortinoco@gmail.com>
2023-03-15 09:05:59 -0300

    feat: Creating component factory and adding base class to model with class-validator

--- src/errors.ts
+++ src/errors.ts
@@ -4,3 +4,10 @@ export class ZendeskClientError extends Error {
     this.name = 'ZendeskClientError';
   }
 }
+
+export class ValidationError extends Error {
+  constructor(message: object) {
+    super(JSON.stringify(message));
+    this.name = 'ValidationError';
+  }
+}
---
+++ src/factories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { useZendesk } from './hooks/use-zendesk';
+import { ZendeskTicketsClient } from './zendesk/tickets';
+import { ZendeskUsersClient } from './zendesk/users';
+
+const dependencesMapper: any = {
+  tickets: ZendeskTicketsClient,
+  users: ZendeskUsersClient
+};
+type Props = {
+  settings: any;
+  Component: React.FC<any>;
+  dependences: any[];
+};
+
+const makeDependences = (zendeskClient: any, dependences: any[]) => {
+  const dependencesObject = {} as any;
+  dependences.forEach((dependence) => {
+    if (!dependencesMapper[dependence])
+      throw new Error(`Dependence ${dependence} not found`);
+
+    dependencesObject[dependence] = new dependencesMapper[dependence](
+      zendeskClient
+    );
+  });
+
+  return dependencesObject;
+};
+
+export const MakeBaseComponent: React.FC<Props> = ({
+  settings,
+  Component,
+  dependences
+}: Props) => {
+  const { zendeskClient } = useZendesk();
+
+  return (
+    <Component
+      settings={settings}
+      {...makeDependences(zendeskClient, dependences)}
+    />
+  );
+};
